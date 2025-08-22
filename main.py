329105825d001eb63b42bcea430201e5caa4307c
Archiet <archiettanwar71@gmail.com>
2025-08-22 23:11:02 +0530

    done finally

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-url=("https://api.nasa.gov/planetary/"
-     "apod?"
-     "api_key=7iUsZAtSHo6Zr4UV6RiWUMmZLJOuQiDPMHeEBD7O")
+url=("https://api.nasa.gov/planetary/apod?"
+     "api_key=B1QDrTTToXr5zj9ehEL21qS9Qnx1nW1BqA8fOLvL")
 
 response=requests.get(url)
 content=response.json()
@@ -13,6 +12,9 @@ print(content)
 print(content["title"])
 print(content["explanation"])
 print(content["date"])
+print(content["copyright"])
+
+col1,col2=st.columns(2)
 
 img_url=content["url"]
 response2=requests.get(img_url)
@@ -20,6 +22,15 @@ img_path="image.jpeg"
 with open(img_path,"wb") as file:
     file.write(response2.content)
 
-st.title(f"{content["title"]}")
-st.image(f"{img_path}")
+
+
+with col1:
+    st.title(f"{content["title"]}")
+    st.image(f"{img_path}")
+
+with col2:
+    st.write(f"{content["date"]}")
+    st.write(f"{content["copyright"]}")
+
 st.write(f"{content["explanation"]}")
+
